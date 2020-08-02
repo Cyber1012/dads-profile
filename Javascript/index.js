4e79decf57c000fbb0539fabf2e5530cdec41e10
Cyber1012 <55567338+Cyber1012@users.noreply.github.com>
2020-08-02 18:39:32 -0400

    Add initial dads-profile website files

---
+++ Javascript/index.js
@@ -0,0 +1,25 @@
+$(".nav-click-me").on("click", function () {
+    alert("I do coding,")
+    alert("so here is one of my work;")
+    alert("I call it my task app.")
+    alert("So you can keep track of all your tasks!")
+})
+
+$(".credit").on("click", function () {
+    alert("This website was specialy made...")
+    alert("...by Bachtiar's own 12 year old son.")
+    alert("So check his website out!")
+})
+
+$(".span-description-eagan").on("mouseover", function () {
+    alert("Who is Eagan you ask?")
+    alert("He is a talentful develop-")
+    alert("Elaine: Just say your his child!")
+    alert("Fine!")
+    alert("I'm his son.")
+})
+
+$(".span-description-elaine").on("mouseover", function () {
+    alert("Who is Elaine you ask?")
+    alert("It's Bachtiar's Daughter")
+})
