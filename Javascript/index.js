3300ef7235e9359e2dbf3478b3ec0e32baac2102
Cyber1012 <55567338+Cyber1012@users.noreply.github.com>
2020-08-04 16:26:49 -0400

    ⏳ Third Update for The Mobile Enhancement Update
    ⏳ Small Batch of Changes as Apart of The Mobile Enhancement Update

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,25 +1,45 @@
 $(".nav-click-me").on("click", function () {
+
     alert("I do coding,")
-    alert("so here is one of my work;")
+    alert("so here is one of the things I've coded;")
     alert("I call it my task app.")
     alert("So you can keep track of all your tasks!")
+
 })
 
 $(".credit").on("click", function () {
+
     alert("This website was specialy made...")
     alert("...by Bachtiar's own 12 year old son.")
     alert("So check his website out!")
+
 })
 
 $(".span-description-eagan").on("mouseover", function () {
+
+    alert("Who is Eagan you ask? He is a talentful develop-")
+    alert("Elaine: Just say your his child!")
+    alert("Fine! I'm his son.")
+
+})
+
+$(".span-description-eagan").on("click", function () {
+
     alert("Who is Eagan you ask?")
     alert("He is a talentful develop-")
     alert("Elaine: Just say your his child!")
     alert("Fine!")
     alert("I'm his son.")
+
 })
 
-$(".span-description-elaine").on("mouseover", function () {
+$(".span-description-elaine").on("mouseover", whoIsElaine)
+
+$(".span-description-elaine").on("click", whoIsElaine)
+
+function whoIsElaine() {
+
     alert("Who is Elaine you ask?")
-    alert("It's Bachtiar's Daughter")
-})
+    alert("She's Bachtiar's Daughter")
+
+}
