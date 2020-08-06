002b5d04d4d237944bd6c92c8a1b2d90820726c5
Cyber1012 <55567338+Cyber1012@users.noreply.github.com>
2020-08-05 20:21:34 -0400

    Update index.js

--- Javascript/index.js
+++ Javascript/index.js
@@ -2,7 +2,6 @@ $(".nav-click-me").on("click", function () {
 
     alert("I do coding, so here is one of the things I've coded;")
     alert("I call it my task app, so you can keep track of all your tasks!")
-    confirm("Do you want to leave the site?")
 
 })
 
@@ -10,7 +9,6 @@ $(".credit").on("click", function () {
 
     alert("This website was specialy made...")
     alert("...by Bachtiar's own 12 year old son. So check his website out!")
-    confirm("Do you want to leave the site?")
 
 })
 
